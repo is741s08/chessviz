0e9d6cb26c58950fa12573853c926abd0934aa32
Иванова Дарья Михайловна <is741s08@mwp12.csc.local>
2018-02-15 09:06:52 +0700

    Add /b

--- chess.c
+++ chess.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    printf("R P 0 0 0 0 P R");
-    printf("k P 0 0 0 0 P k");
-    printf("B P 0 0 0 0 P B");
-    printf("Q P 0 0 0 0 P Q");
-    printf("K P 0 0 0 0 P K");
-    printf("B P 0 0 0 0 P B");
-    printf("k P 0 0 0 0 P k");
-    printf("R P 0 0 0 0 P R");
+    printf("R P 0 0 0 0 P R"/b);
+    printf("k P 0 0 0 0 P k"/b);
+    printf("B P 0 0 0 0 P B"/b);
+    printf("Q P 0 0 0 0 P Q"/b);
+    printf("K P 0 0 0 0 P K"/b);
+    printf("B P 0 0 0 0 P B"/b);
+    printf("k P 0 0 0 0 P k"/b);
+    printf("R P 0 0 0 0 P R"/b);
     
     return 0;
 }
