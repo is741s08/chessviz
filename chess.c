bd20987f878934931d2fd3609f801e8dc60bed4d
Иванова Дарья Михайловна <is741s08@mwp12.csc.local>
2018-02-15 08:40:44 +0700

    Create chess.c

---
+++ chess.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("R P 0 0 0 0 P R");
+    printf("k P 0 0 0 0 P k");
+    printf("B P 0 0 0 0 P B");
+    printf("Q P 0 0 0 0 P Q");
+    printf("K P 0 0 0 0 P K");
+    printf("B P 0 0 0 0 P B");
+    printf("k P 0 0 0 0 P k");
+    printf("R P 0 0 0 0 P R");
+    
+    return 0;
+}
