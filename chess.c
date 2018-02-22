419eec2dfe368c84196916804af6e01cc1560a21
Иванова Дарья Михайловна <is741s08@mwp12.csc.local>
2018-02-22 09:06:51 +0700

    Change vision

--- chess.c
+++ chess.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    printf("R P 0 0 0 0 P R\n");
-    printf("k P 0 0 0 0 P k\n");
-    printf("B P 0 0 0 0 P B\n");
-    printf("Q P 0 0 0 0 P Q\n");
-    printf("K P 0 0 0 0 P K\n");
-    printf("B P 0 0 0 0 P B\n");
-    printf("k P 0 0 0 0 P k\n");
-    printf("R P 0 0 0 0 P R\n");
+    printf("8 r n b q k b n r\n");
+    printf("7 p p p p p p p p\n");
+    printf("6                \n");
+    printf("5                \n");
+    printf("4                \n");
+    printf("3                \n");
+    printf("2 P P P P P P P P\n");
+    printf("1 R N B Q K B N R\n");
+    printf("  a b c d e f g h\n");
     
     return 0;
 }
